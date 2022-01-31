1adf76368ebd57a2de4efbfedff690b51cf4c03c
Anthony <adominguez15722@gmail.com>
2022-01-31 15:18:58 -0600

    Artist SQL complete

---
+++ artist.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO artist(name)
+-- VALUES('Nelly'),
+-- ('Ludacris'), 
+-- ('Sheryl Crow');
+
+-- SELECT * FROM artist;
+
+-- SELECT * FROM artist
+-- ORDER BY name DESC
+-- LIMIT 10;
+
+-- SELECT * FROM artist
+-- ORDER BY name ASC
+-- LIMIT 5;
+
+-- SELECT * FROM artist
+-- WHERE name ILIKE 'BLACK%'; 
+
+-- SELECT * FROM artist
+-- WHERE name ILIKE '%black%'; 

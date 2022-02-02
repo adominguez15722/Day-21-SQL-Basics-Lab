f0915025b89460ac39690235b411b2b9a109200b
Anthony <adominguez15722@gmail.com>
2022-02-01 21:28:00 -0600

    Update SQL complete

---
+++ update.sql
@@ -0,0 +1,30 @@
+-- UPDATE customer
+-- SET fax = NULL
+-- WHERE fax IS NULL;
+
+-- UPDATE customer
+-- SET company = 'Self'
+-- WHERE company IS NULL;
+
+-- SELECT * FROM customer;
+
+-- UPDATE customer
+-- SET last_name = 'Thompson'
+-- WHERE first_name = 'Julia'
+-- AND last_name = 'Barnett';
+
+-- SELECT * FROM customer;
+
+-- UPDATE customer
+-- SET support_rep_id = 4
+-- WHERE email = 'luisrojas@yahoo.cl';
+
+-- SELECT * FROM genre;
+
+-- UPDATE track
+-- SET composer = 'The darkness around us'
+-- WHERE genre_id = 3
+-- AND composer IS NULL;
+
+-- SELECT * FROM track
+-- WHERE genre_id = 3;

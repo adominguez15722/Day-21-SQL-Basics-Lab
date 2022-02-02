683b78c690ea0c78cb12f6072515636ce93c5c05
Anthony <adominguez15722@gmail.com>
2022-02-01 22:00:01 -0600

    Delete SQL complete

--- delete.sql
+++ delete.sql
@@ -8,3 +8,29 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+-- SELECT * FROM animals;
+
+-- SELECT * FROM animals
+-- WHERE type = 'lion';
+
+-- DELETE
+-- FROM animals
+-- WHERE type = 'lion';
+
+-- SELECT * FROM animals;
+
+-- SELECT * FROM animals
+-- WHERE name ILIKE 'm%';
+
+-- DELETE
+-- FROM animals
+-- WHERE name ILIKE 'm%';
+
+-- SELECT * FROM animals;
+
+-- DELETE
+-- FROM animals
+-- WHERE age < 9;
+
+-- SELECT * FROM animals;
